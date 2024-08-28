5ada4692a141da5588f49b2ebe37baeea88cb06b
Ruan-cmd <ruan4.t@gmail.com>
2024-08-29 00:40:14 +0200

    Add files via upload

---
+++ Group50_Hotel_System/Group50_Hotel_System/Login.cs
@@ -0,0 +1,30 @@
+﻿using Group_50_CMPG223_HotelManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group50_Hotel_System
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+    }
+}
---
+++ Group50_Hotel_System/Group50_Hotel_System/SessionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_50_CMPG223_HotelManagementSystem
+{
+    public static class SessionManager
+    {
+        public static int LoggedInEmployeeID { get; set; }
+        public static string LoggedInEmployeeUsername { get; set; }
+        public static string LoggedInEmployeeName { get; set; }
+        public static string LoggedInEmployeeSurname { get; set; }
+
+        public static string ConnectionString { get; set; } =
+            "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Cmpg223\\Group50\\Group50_Hotel_System\\Group50_Hotel_System\\HotelManagementSystem.mdf;Integrated Security=True";
+    }
+}
