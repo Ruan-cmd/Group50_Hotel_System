5eac390bc6ebe819052930976808c6e523d1c7ed
JanreO <147954035+JanreO@users.noreply.github.com>
2024-09-01 16:08:41 +0200

    Commit

--- Group50_Hotel_System/Group50_Hotel_System/SessionManager.cs
+++ Group50_Hotel_System/Group50_Hotel_System/SessionManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Group_50_CMPG223_HotelManagementSystem
+﻿namespace Group_50_CMPG223_HotelManagementSystem
 {
     public static class SessionManager
     {
@@ -12,6 +6,8 @@ namespace Group_50_CMPG223_HotelManagementSystem
         public static string LoggedInEmployeeUsername { get; set; }
         public static string LoggedInEmployeeName { get; set; }
         public static string LoggedInEmployeeSurname { get; set; }
+
+        public static string LoggedInEmployeeRole { get; set; }
 
         public static string ConnectionString { get; set; } =
             "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\OneDrive\\Documents\\GITHUB\\RuanCMD\\Group50_Hotel_System\\Group50_Hotel_System\\Group50_Hotel_System\\HotelManagementSystem.mdf;Integrated Security=True";
